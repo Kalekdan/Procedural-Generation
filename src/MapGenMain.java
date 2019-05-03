990299a7dae791943006cf5e19cfac9a5f643ce2
Joseph Rickard <joerickard@hotmail.co.uk>
2019-05-03 14:33:22 +0100

    Added random height generation

--- src/MapGenMain.java
+++ src/MapGenMain.java
@@ -3,7 +3,7 @@ public class MapGenMain {
     public static void main(String[] args) {
         MapGrid map = new MapGrid(15,20);
 
-        map.InitialGenerateMap(0.9f,0);
+        map.InitialGenerateMap(0.9f,2, 10);
 
         System.out.println(map.toString());
     }
--- src/MapGrid.java
+++ src/MapGrid.java
@@ -17,10 +17,10 @@ public class MapGrid {
      * @param waterPercent    approximate percentage of the map to be water
      * @param heightVariation approximate aferage difference between lowest point and highest
      */
-    public void InitialGenerateMap(float waterPercent, float heightVariation) {
+    public void InitialGenerateMap(float waterPercent, float minHeight, float maxHeight) {
         for (int i = 0; i < xSize; i++) {
             for (int j = 0; j < ySize; j++) {
-                gridPoints[i][j] = new GridPoint(GenerateTerrainType(waterPercent), GenerateTerrainHeight());
+                gridPoints[i][j] = new GridPoint(GenerateTerrainType(waterPercent), GenerateTerrainHeight(minHeight, maxHeight));
             }
         }
     }
@@ -28,25 +28,26 @@ public class MapGrid {
     /**
      * Tidies up the map to make it more natural after the random height and terrain generation
      */
-    public void SmoothMap() {
+    public void SmoothMap(float maxAdjacentHeightDiff, float avgAdjacentHeightDiff) {
         SmoothMapTerrainTypes();
-        SmoothMapHeights();
+        SmoothMapHeights(maxAdjacentHeightDiff, avgAdjacentHeightDiff);
     }
 
     private String GenerateTerrainType(float percentWater) {
         Random r = new Random();
         if (r.nextFloat() > percentWater){
-            return "land";
+            return "l";
         } else {
-            return "water";
+            return "w";
         }
     }
 
-    private float GenerateTerrainHeight() {
-        return 0f;
+    private float GenerateTerrainHeight(float max, float min) {
+        Random r = new Random();
+        return min + r.nextFloat() * (max - min);
     }
 
-    private void SmoothMapHeights() {
+    private void SmoothMapHeights(float maxDiff, float avgDiff) {
 
     }
 
@@ -68,7 +69,7 @@ public class MapGrid {
         String toReturn = "";
         for (int i = 0; i < ySize; i++) {
             for (int j = 0; j < xSize; j++) {
-                toReturn += gridPoints[j][i].toString() + "\t";
+                toReturn += gridPoints[j][i].toString() + "\t\t";
             }
             toReturn += "\n";
         }
