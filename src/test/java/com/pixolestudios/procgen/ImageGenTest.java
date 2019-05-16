76ee5f9fb86c867c0d6d9c70468b72f14453920e
jrickard <joerickard@hotmail.co.uk>
2019-05-16 09:58:13 +0100

    Added ImageGen tests #18

---
+++ src/test/java/com/pixolestudios/procgen/ImageGenTest.java
@@ -0,0 +1,50 @@
+package com.pixolestudios.procgen;
+
+
+import main.java.com.pixolestudios.procgen.ImageGen;
+import main.java.com.pixolestudios.procgen.MapGrid;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageGenTest {
+
+    private final String testOutLoc = "src/test/java/com/pixolestudios/procgen/testout";
+
+    @Test
+    public void assertImageResolutionAsExpected() {
+        int mapXSize = 10, mapYSize = 20;
+        int pixelsPerGridPoint = 50;
+
+        MapGrid mapToImage = new MapGrid(mapXSize, mapYSize);
+        mapToImage.InitialGenerateDryMap(1, 255);
+
+        ImageGen img = new ImageGen(mapToImage, testOutLoc + "/image.png", pixelsPerGridPoint);
+        img.GenerateImg();
+
+        BufferedImage bimg = null;
+        try {
+            bimg = ImageIO.read(new File(testOutLoc + "/image.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int actualXSize = bimg.getWidth();
+        int actualYSize = bimg.getHeight();
+
+        Assert.assertEquals(mapXSize * pixelsPerGridPoint, actualXSize);
+        Assert.assertEquals(mapYSize * pixelsPerGridPoint, actualYSize);
+    }
+
+    @Test
+    public void imageGenOnUninitiatedMapDoesntError() {
+        int mapXSize = 10, mapYSize = 20;
+        MapGrid mapToImage = new MapGrid(mapXSize, mapYSize);
+
+        ImageGen img = new ImageGen(mapToImage, testOutLoc + "/image.png", 1);
+        img.GenerateImg();
+    }
+}
