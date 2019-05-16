6ca2bc912d3e7f7d066d93dfb8362ce0f1e698c3
jrickard <joerickard@hotmail.co.uk>
2019-05-16 11:36:09 +0100

    Updated ImageGen tests #18

--- src/test/java/com/pixolestudios/procgen/ImageGenTest.java
+++ src/test/java/com/pixolestudios/procgen/ImageGenTest.java
@@ -23,12 +23,12 @@ public class ImageGenTest {
         MapGrid mapToImage = new MapGrid(mapXSize, mapYSize);
         mapToImage.InitialGenerateDryMap(1, 255);
 
-        ImageGen img = new ImageGen(mapToImage, testOutLoc + "/image.png", pixelsPerGridPoint);
+        ImageGen img = new ImageGen(mapToImage, testOutLoc + "/assertImageResolutionAsExpected.png", pixelsPerGridPoint);
         img.GenerateImg();
 
         BufferedImage bimg = null;
         try {
-            bimg = ImageIO.read(new File(testOutLoc + "/image.png"));
+            bimg = ImageIO.read(new File(testOutLoc + "/assertImageResolutionAsExpected.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -40,11 +40,14 @@ public class ImageGenTest {
     }
 
     @Test
-    public void imageGenOnUninitiatedMapDoesntError() {
+    public void imageGenOnUninitializedMapDoesntMakeImage() {
         int mapXSize = 10, mapYSize = 20;
         MapGrid mapToImage = new MapGrid(mapXSize, mapYSize);
 
-        ImageGen img = new ImageGen(mapToImage, testOutLoc + "/image.png", 1);
+        ImageGen img = new ImageGen(mapToImage, testOutLoc + "/imageGenOnUninitiatedMapDoesntMakeImage.png", 1);
         img.GenerateImg();
+
+        File output = new File(testOutLoc + "/imageGenOnUninitiatedMapDoesntMakeImage.png");
+        Assert.assertFalse("Image was created on uninitialised map.", output.exists());
     }
 }
