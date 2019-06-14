181e08519353ec0bf99a302e9eb146f633fa49a1
Joseph Rickard <joerickard@hotmail.co.uk>
2019-06-14 10:17:47 +0100

    Added option to export image #39

--- src/main/java/com/pixolestudios/uiUtils/ImgWindow.java
+++ src/main/java/com/pixolestudios/uiUtils/ImgWindow.java
@@ -7,9 +7,15 @@ import main.java.com.pixolestudios.procgen.MapGrid;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -24,9 +30,17 @@ public class ImgWindow extends JFrame {
     private static final int pixelsPerGrid = 10;
 
     private ImageGen imgGen;
+    private MapGrid map;
     private JLabel picLabel;
 
-    public ImgWindow(){
+    private JMenuBar mnub_MenuBar;
+    private JMenu mnu_fileMenu;
+    private JMenuItem mnui_save;
+
+    private JFileChooser fileChooser;
+    private String savePath = "";
+
+    public ImgWindow() {
         setupWindow();
         setupWindowContents();
         setLayout(null);
@@ -42,19 +56,59 @@ public class ImgWindow extends JFrame {
 
     private void setupWindowContents() {
         picLabel = new JLabel();
+        setupSave();
     }
 
     protected void addImageToView(MapGrid mapToImg) throws IOException, UninitializedMapException {
-        imgGen = new ImageGen(mapToImg, pixelsPerGrid);
+        map = mapToImg;
+        imgGen = new ImageGen(map, pixelsPerGrid);
         imgGen.GenerateImg();
         BufferedImage img = ImageIO.read(new File(ImageGen.DEFAULT_OUT_LOC));
         picLabel.setIcon(new ImageIcon(img));
-        picLabel.setBounds(0, 0, mapToImg.getXSize() * pixelsPerGrid, mapToImg.getYSize() * pixelsPerGrid);
-        getContentPane().setPreferredSize(new Dimension(mapToImg.getXSize() * pixelsPerGrid, mapToImg.getYSize() * pixelsPerGrid));
+        picLabel.setBounds(0, 0, map.getXSize() * pixelsPerGrid, map.getYSize() * pixelsPerGrid);
+        getContentPane().setPreferredSize(new Dimension(map.getXSize() * pixelsPerGrid, map.getYSize() * pixelsPerGrid));
         add(picLabel);
         picLabel.setVisible(true);
         repaint();
         pack();
+    }
+
+    private void setupSave() {
+        mnui_save = new JMenuItem("Save Image");
+        mnui_save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    doSaveImageEvent();
+                } catch (UninitializedMapException e1) {
+                    e1.printStackTrace();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        mnu_fileMenu = new JMenu("File");
+        mnu_fileMenu.add(mnui_save);
+
+        mnub_MenuBar = new JMenuBar();
+        mnub_MenuBar.add(mnu_fileMenu);
+
+        setJMenuBar(mnub_MenuBar);
+    }
+
+    private void doSaveImageEvent() throws UninitializedMapException, IOException {
+        fileChooser = new JFileChooser();
+        int r = fileChooser.showSaveDialog(null);
+        // if the user selects a file
+        if (r == JFileChooser.APPROVE_OPTION) {
+            savePath = fileChooser.getSelectedFile().getAbsolutePath();
+            PLog.info("Saving image at " + savePath);
+            ImageGen imgSave = new ImageGen(map, savePath, pixelsPerGrid);
+            imgSave.GenerateImg();
+        } else {
+            PLog.debug("Save image cancelled");
+        }
     }
 
 }
